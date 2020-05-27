414a163851c8cc53bdb9323a538f25150965ab0e
Ryan Booh <macuser@Macs-MacBook-Pro.local>
2020-05-26 20:13:17 -0400

    Updates

---
+++ precache-manifest.46ca189982f89b602e584af34071cbc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e14dfa06e9c993a4ed68218492e605",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "9b7160b07968240ec051",
+    "url": "/airbnb-clone/static/css/main.4b72ad2f.chunk.css"
+  },
+  {
+    "revision": "b93a37dd014cccc903ea",
+    "url": "/airbnb-clone/static/js/2.1f0f73d8.chunk.js"
+  },
+  {
+    "revision": "9b7160b07968240ec051",
+    "url": "/airbnb-clone/static/js/main.3d788e1d.chunk.js"
+  },
+  {
+    "revision": "ad35de0ef885a5c7628e",
+    "url": "/airbnb-clone/static/js/runtime-main.c1135073.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/airbnb-clone-react/precache-manifest.7caaca93de46c00bf1172ebacb6635b5.js"
+  "/airbnb-clone/precache-manifest.46ca189982f89b602e584af34071cbc8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/airbnb-clone-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/airbnb-clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
