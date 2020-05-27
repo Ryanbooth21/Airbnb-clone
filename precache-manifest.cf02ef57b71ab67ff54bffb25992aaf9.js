8018d8b2514ee55a70b36efa5c31447706b1f7f8
Ryan Booh <macuser@Macs-MacBook-Pro.local>
2020-05-26 20:08:51 -0400

    Updates

---
+++ precache-manifest.cf02ef57b71ab67ff54bffb25992aaf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1aa5844cc2c1988f1be1235c0ec954",
+    "url": "/airbnb-react/index.html"
+  },
+  {
+    "revision": "8e94f8465e0473dcaa88",
+    "url": "/airbnb-react/static/css/main.4b72ad2f.chunk.css"
+  },
+  {
+    "revision": "b93a37dd014cccc903ea",
+    "url": "/airbnb-react/static/js/2.1f0f73d8.chunk.js"
+  },
+  {
+    "revision": "8e94f8465e0473dcaa88",
+    "url": "/airbnb-react/static/js/main.ccb90937.chunk.js"
+  },
+  {
+    "revision": "f62822742616bc69c14d",
+    "url": "/airbnb-react/static/js/runtime-main.41ebf555.js"
+  }
+]);
